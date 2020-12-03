200548d3badbd429fb533e9436b20d04e5a203fa
menachem m margolis <margolismendel5728@gmail.com>
2020-12-03 15:28:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,29 @@
 # Write your code here!
+def game_hash
+  kkk={
+  :home=>{
+      :team_name=>"brooklyn nets",
+      :colors=>["black","white"],
+      :players=>[
+              :alan_anderson=>{:player_name=>"Alan Anderson",:Number=>0,:Shoe=>16, :points=>22,:Rebounds=>12,:Assits=>12, :Steals=>3,:Blocks=>1,:slam_unks=>1},
+              :reggie_evans=>{:player_name=>"Reggie Evans",:Number=>30,:Shoe=>14,:points=>12,:Rebounds=>12,:Assits=>12,:Steals=>12,:Blocks=>12,:Slam_dunks=>7},
+              :brook_lopez=>{:player_name=>"Brook Lopez",:Number=>11,:Shoe=>17,:points=>17,:Rebounds=>19,:Assits=>10,:Steals=>1,:Blocks=>3,:Slam_dunks=>15},
+              :mason_plumlee=>{:player_name=>"Mason Plumlee",:Number=>1,:Shoe=>19,:points=>26,:Rebounds=>11,:Assits=>6,:Steals=>3,:Blocks=>8,:Slam_dunks=>5},
+              :jason_terry=>{:player_name=>"Jason Terry",:Number=>31,:Shoe=>15,:points=>19,:Rebounds=>2,:Assits=>2,:Steals=>4,:Blocks=>11,:Slam_dunks=>1}
+      ]
+  },
+:away=>{
+    :team_name=>"Charlotte Hornets",
+    :colors=>["Turquoise","Purple"],
+    :players=>[
+      :jeff_adrien=>{:player_name=>"Jeff Adrian", :Number=>4,:Shoe=>18,:points=10,:Rebounds=>1,:Assits=>1, :Steals=>2,:Blocks=>0,:slam_unks=>2},
+      :bismack_biyombo=>{:player_name=>"Bismack Biyombo",:Number=>0,:Shoe=>16,:points=>12,:Rebounds=>4,:Assits=>7,:Steals=>22,:Blocks=>15,:Slam_dunks=>10},
+      :desagna_diop=>{:player_name=>"DeSagna Diop",:Number=>2,:Shoe=>14,:points=>24,:Rebounds=>12,:Assits=>12,:Steals=>4,:Blocks=>5,:Slam_dunks=>5},
+      :ben_gordon=>{:player_name=>"Ben Gordon",:Number=>8,:Shoe=>15,:points=>33,:Rebounds=>3,:Assits=>2,:Steals=>1,:Blocks=>1,:Slam_dunks=>0},
+      :kemba_walker=>{:player_name=>"Kemba Walker",:Number=>33,:Shoe=>15,:points=>6,:Rebounds=>12,:Assits=>12,:Steals=>7,:Blocks=>5,:Slam_dunks=>12}
+    ]
+
+
+ }
+}
+end
